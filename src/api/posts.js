1f294e8fe46eb62d2a4a549b34d8b14ab001f076
seungyong88 <gosng2001@naver.com>
2020-04-01 19:31:19 +0900

    스플리팅 전 push

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,50 +1,32 @@
-import axios from 'axios';
+// n 밀리 세컨드 동안 기다리는 프로미스를 만들어주는 함수
+const sleep = n => new Promise(resolve => setTimeout(resolve, n));
 
-// n 밀리세컨드동안 기다리는 프로미스를 만들어주는 함수
-// const sleep = n => new Promise(resolve => setTimeout(resolve, n))
-const sleep = n => new Promise(reslove => setTimeout(reslove, n));
-
-// 가짜 포스트 목록 데이터 
-
+// 가짜 포스트 목록 데이터
 const posts = [
   {
     id: 1,
-    title: '리덕스 미들웨어를 배워봅시다',
-    body: '리덕스 미들웨어를 직접 만들어보면 이해하기 쉽죠.'
+    title: '리덕스 미들웨어를 배워본다.',
+    body: '리덕스 미들웨어를 직접 만들어보면 이해가 쉽다나..'
   },
   {
     id: 2,
-    title: 'redux-thunk를 사용해봅시다',
-    body: 'redux-thunk를 사용해서 비동기 작업을 처리해봅시다!'
+    title: 'redux-thunk를 사용해본다.',
+    body: '리덕스 미들웨어를 직접 만들어보면 이해가 쉽다나..'
   },
   {
     id: 3,
-    title: 'redux-saga도 사용해봅시다',
-    body:
-      '나중엔 redux-saga를 사용해서 비동기 작업을 처리하는 방법도 배워볼 거예요.'
-  }
-];
+    title: '리덕스 미들웨어를 배워본다.',
+    body: '리덕스 미들웨어를 직접 만들어보면 이해가 쉽다나..'
+  },
+]
 
-// //포스트 목록을 가져오는 비동기 함수
-// export const getPosts = async () => {
-//   await sleep(500);
-//   return posts;
-// }
-
-// // ID로 포스트 목록을 가져오는 비동기 함수
-// export const getPostById = async id => {
-//   await sleep(500);
-//   return posts.find(post => post.id === id); // id 로 찾아서 반환
-// }
-
+// 포스트 목록을 가져오는 비동기 함수
 export const getPosts = async () => {
-  const response = await axios.get('/posts');
-  console.log(response);
-  return response.data;
+  await sleep(500); // 0.5초 쉬고
+  return posts;
 }
 
-
 export const getPostById = async id => {
-  const response = await axios.get(`/posts/${id}`);
-  return response.data;
-};
+  await sleep(500); // 0.5 초 쉬고
+  return posts.find(post => post.id === id); // id로 찾아서 반환
+}
--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -2,24 +2,16 @@ import React from 'react';
 import Counter from '../components/Counter';
 import { useSelector, useDispatch } from 'react-redux';
 import { increase, decrease } from '../modules/counter';
-import { increaseAsync, decreaseAsync } from '../modules/counter';
 
 function CounterContainer() {
   const number = useSelector(state => state.counter);
   const dispatch = useDispatch();
 
-  // const onIncrease = () => {
-  //   dispatch(increase());
-  // };
-  // const onDecrease = () => {
-  //   dispatch(decrease());
-  // };
-
   const onIncrease = () => {
-    dispatch(increaseAsync());
+    dispatch(increase());
   };
   const onDecrease = () => {
-    dispatch(decreaseAsync());
+    dispatch(decrease());
   };
 
   return (
