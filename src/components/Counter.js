403f0be8fe0642289aa5776eeabd66a9747a9ba7
seungyong88 <gosng2001@naver.com>
2020-04-02 14:41:48 +0900

    ??

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,7 +4,7 @@ function Counter({ number, onIncrease, onDecrease }) {
   return (
     <div>
       <h1>{number}</h1>
-      <button onClick={onIncrease}>+1</button>
+      <button onClick={onIncrease}>+11</button>
       <button onClick={onDecrease}>-1</button>
     </div>
   );
